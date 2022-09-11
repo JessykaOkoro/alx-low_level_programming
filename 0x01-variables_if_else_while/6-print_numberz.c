6720d91239a3cb28b651811a5176e9dec4c44126
JessykaOkoro <jessicaokoroostra@gmail.com>
2022-09-11 16:28:38 -0700

    Ubuntu loading

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - "Determines if a number is positive, negative or zero."
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	/*Declare variables here*/
+	int n;
+
+	/*Write statements here*/
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n % 10 + '0');
+	}
+	putchar('\n');
+	return (0);
+
+}
