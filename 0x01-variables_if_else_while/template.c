4e3f8e625c731db4632df4386d7c458854eb6a57
JessykaOkoro <jessicaokoroostra@gmail.com>
2022-09-11 15:12:16 -0700

    I'm on call

---
+++ 0x01-variables_if_else_while/template.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - "Determines if a number is positive, negative or zero."
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	/*Declare variables here*/
+
+	/*Write statements here*/
+
+}
