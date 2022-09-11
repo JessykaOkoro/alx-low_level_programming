11a86bbb53a7d11d568bd978381bd5b01f5faeb1
JessykaOkoro <jessicaokoroostra@gmail.com>
2022-09-11 16:42:33 -0700

    We are good

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - "Determines if a number is positive, negative or zero."
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	/*Declare variables here*/
+	char az;
+
+	/*Write statements here*/
+	for (az = 'z'; az >= 'a'; az--)
+	{
+		putchar(az);
+	}
+	putchar('\n');
+	return (0);
+}
