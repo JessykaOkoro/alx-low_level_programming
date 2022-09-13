2e9388e9a7b71ed5df1c6ac2f9bebf4fc3308f84
JessykaOkoro <jessicaokoroostra@gmail.com>
2022-09-13 12:45:44 -0700

    0-putchar.c

---
+++ 0x01-variables_if_else_while/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - "Determines if a number is positive, negative or zero."
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	/*Declare variables here*/
+	char az;
+
+	/*Write statements here*/
+	for (az = 'a'; az <= 'z'; az++)
+	{
+		putchar(az);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ pld/0-positive_or_negative.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/*
+ * main - determine if the code is positive or negative
+ *
+ *
