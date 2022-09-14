1395ee0dcbe5347ede25ae93ab8364bd5bcd34ad
JessykaOkoro <jessicaokoroostra@gmail.com>
2022-09-14 14:46:31 -0700

    first werey

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,20 +3,20 @@
 /**
  * main - writes the character c to stdout
  *
- * Return: on success 1
- * On error -1 is returned and errno is set appropriately
+ * Return: on success 0
+ *
  */
 int main(void)
 {
-	putchar('_')
-	putchar('p')
-	putchar('u')
-	putchar('t')
-	putchar('c'
-	putchar('h')
-	putchar('a')
-	putchar('r')
-	putchar('\n')
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 	return (0);
 }
 
