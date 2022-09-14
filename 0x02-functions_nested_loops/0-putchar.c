e4a70e79cf213a7cb16a18b49e971fe5da06890f
JessykaOkoro <jessicaokoroostra@gmail.com>
2022-09-13 21:49:09 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,20 @@
 #include <main.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _putchar - prints nane
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: Always 0
+ *
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	char *sh = "_putchar";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+	return (0);
 }
