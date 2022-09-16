497057f368f421ebbc1f0ded4ac0f7289eb8e41c
JessykaOkoro <jessicaokoroostra@gmail.com>
2022-09-15 17:39:24 -0700

    3

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * 3main b_day - converts day of month to day of year, without accounting
+ * convert_day - converts day of month to day of year, without accounting
  * for leap year
  * @month: month in number format
  * @day: day of month
