cc05e17667a8b1980261a44be94e2fd2461272e3
JessykaOkoro <jessicaokoroostra@gmail.com>
2022-09-19 13:23:35 -0700

    100-main.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -14,7 +14,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-int main(void)
 void rev_string(char *s);
 
 #endif /* MAIN_H */ 
