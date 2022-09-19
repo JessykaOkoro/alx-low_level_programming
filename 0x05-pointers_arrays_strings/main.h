6a79d4857c4baaa289be191d555cca3a83b81274
JessykaOkoro <jessicaokoroostra@gmail.com>
2022-09-19 10:47:25 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -14,7 +14,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-void rev_string(char *s)
-char *_strcpy(char *dest, char *src);
+void rev_string(char *s);
 
 #endif /* MAIN_H */ 
