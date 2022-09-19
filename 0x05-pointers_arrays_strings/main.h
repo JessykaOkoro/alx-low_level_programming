3766b9e9f89dbc980d4d739db15bf1c92c2b6f00
JessykaOkoro <jessicaokoroostra@gmail.com>
2022-09-19 07:50:38 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -14,4 +15,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#endif 
+#endif /* MAIN_H */ 
